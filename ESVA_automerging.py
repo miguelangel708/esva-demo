9db63cf4c525ac72798814b2dbb0b2e548cf63a6
miguelangel708 <mimunoza@unal.edu.co>
2024-03-08 14:56:18 -0500

    fix page list error

--- ESVA_automerging.py
+++ ESVA_automerging.py
@@ -84,16 +84,16 @@ def process_answer(query):
 
         response = query_engine.query(query)
         source_documents = response.source_nodes[0].metadata['file_name']
-        page_list = [int(i.metadata['page_label'])
-                     for i in response.source_nodes]
+        # page_list = [int(i.metadata['page_label'])
+        #              for i in response.source_nodes]
         # page_list = sorted(list(set(page_list)))
         # page_list = ', '.join(map(str, set(page_list)))
-        page_list.sort()
-        if (source_documents == "Instructivo_Diligenciamiento_Formato_Reporte_de_Eficiencia_de_Combustion_en_T_1PDJHb8.pdf"):
-            source_documents = "Instructivo_Diligenciamiento_1PDJHb8"
-        pass
+        # page_list.sort()
+        # if (source_documents == "Instructivo_Diligenciamiento_Formato_Reporte_de_Eficiencia_de_Combustion_en_T_1PDJHb8.pdf"):
+        #     source_documents = "Instructivo_Diligenciamiento_1PDJHb8"
+        # pass
     except AuthenticationError as e:
         print(e)
         return "llave open ai invalida"
 
-    return response.response + f' \n Documento Fuente: {source_documents} \n Paginas: {page_list}'
+    return response.response + f' \n Documento Fuente: {source_documents} '
